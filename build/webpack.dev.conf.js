31bcd1d7c4abeddc1651e5d37f7860763c5bc866
MZI <macchenjl@foxmail.com>
2018-08-17 17:53:32 +0800

    Update packages && Add bundle analyzer

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,6 +9,7 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const portfinder = require('portfinder')
 
 
@@ -67,7 +68,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ]
 })
 
